898dc30f0f2c4a6ee43c34b14f5b9c3d510c54d7
Guido Accardo <gaccardo@gmail.com>
2018-02-22 14:01:38 +0000

    call crt creation

--- src/api/tools/openvpn.sh
+++ src/api/tools/openvpn.sh
@@ -3,4 +3,7 @@ cd /etc/openvpn/openvpn-ca
 source ./vars
 openssl req -nodes -newkey rsa:2048 -keyout $1.key -out $1.csr \
   -subj "/C=US/ST=BuenosAires/L=BuenosAires/O=SimpleVPN/OU=IT/CN=simplevpn.com"
+openssl ca -days 3650 -out $1.crt -in $1.cst \
+  -subj "/C=US/ST=BuenosAires/L=BuenosAires/O=SimpleVPN/OU=IT/CN=simplevpn.com"
 mv $1.* keys/
+
